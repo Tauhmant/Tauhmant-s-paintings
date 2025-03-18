d9fe9c4ab0f80b177d9714bf620fa1fa29eb9ac1
Tauhmant <86077546+Tauhmant@users.noreply.github.com>
2025-03-18 15:42:06 -0500

    Add files via upload

--- test1/obj/Debug/netstandard2.1/MyPluginInfo.cs
+++ test1/obj/Debug/netstandard2.1/MyPluginInfo.cs
@@ -4,6 +4,6 @@ namespace test1
     {
         public const string PLUGIN_GUID = "test1";
         public const string PLUGIN_NAME = "test1";
-        public const string PLUGIN_VERSION = "1.0.0";
+        public const string PLUGIN_VERSION = "1.1.0";
     }
 }
